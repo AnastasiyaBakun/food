92a074fb2fcc911a6f0f45546051b931335acc75
Bakun Anastasiya <bakun_an_20@mf.grsu.by>
2023-05-20 16:23:07 +0300

    Скрипты для 10 лабораторной

---
+++ Лаба 10/Заполнение_таблицы_данными.sql
@@ -0,0 +1,4 @@
+INSERT INTO test_table (tbl_name, tbl_date, tbl_description) VALUES
+('first', '2023-05-18', 'bla bla bla'),
+('second', '2023-05-17', 'bla bla');
+SELECT * FROM test_table;
---
+++ Лаба 10/Создание_таблицы.sql
@@ -0,0 +1,6 @@
+CREATE TABLE test_table (
+tbl_id INT NOT NULL AUTO_INCREMENT,
+tbl_name VARCHAR(30),
+tbl_date DATE,
+tbl_description VARCHAR(50),
+PRIMARY KEY (tbl_id));
